10b24a50c4ff2acd59c9634691c05baf5679b663
Vladislav_Sharay <vladislav.sharay@gmail.com>
2019-07-15 18:23:07 +0200

    add img

--- homework3/Slider/index.js
+++ homework3/Slider/index.js
@@ -1,20 +1,59 @@
 
-var dropdownBtn=document.querySelector("#dropdown");
+var fragToggle = document.createDocumentFragment();
 
-var dropdownCnt=document.querySelector("#content");
+var sectionToggle = document.createElement("section");
+sectionToggle.id = "section";
+
+var button1 = document.createElement("div");
+button1.id = "button1";
+button1.innerText="\u25c4";
+var button2 = document.createElement("div");
+button2.id = "button2";
+button2.innerText="\u25ba";
+
+var img1 = document.createElement("img");
+img1.src="/img/1"; 
+var img1 = document.createElement("img");
+img1.src="/img/2"; 
+var img1 = document.createElement("img");
+img1.src="/img/3"; 
+
+var span = document.createElement("span");
+span.id="close"; 
 
 
-dropdownCnt.style.display= "none" ;
+var cnt = document.createElement("p");
+cnt.id="cnt"; 
 
 
-dropdownBtn.addEventListener("click", function() {
-    if (dropdownCnt.style.display == "none")
 
-   {dropdownCnt.style.display = "block"}
+fragToggle.appendChild(sectionToggle);
+sectionToggle.appendChild(button);
+sectionToggle.appendChild(modal);
+modal.appendChild(span);
+modal.appendChild(cnt);
 
-else
 
-   {dropdownCnt.style.display = "none"}
+document.body.appendChild(fragToggle);
 
-})
+modal.style.display = "none";
 
+
+button.innerText = 'Click Me';
+span.innerText="\u2716";
+cnt.innerText="Здесь какой-то текст";
+
+
+button.onclick = function() {
+    modal.style.display = "block";
+  
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }}
