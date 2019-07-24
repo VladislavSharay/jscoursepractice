54114372b8f5e3a28143581c1de728b3a0419a67
Vladislav_Sharay <vladislav.sharay@gmail.com>
2019-07-24 14:00:58 +0300

    add heroku

--- homework5/registration.js
+++ homework5/registration.js
@@ -13,45 +13,49 @@ formSignUp.addEventListener('submit', event => {
     let opt = 'signUp';
 
     doRequest(url, method, form, opt)
-    .then(() => {
-        if(tokenStr != undefined) {
-            let url = API + '/activate';
-            let form = JSON.stringify({ id: tokenStr });
-            let opt = 'activate';
-            doRequest(url, method, form, opt)
-                .then(() => tokenStr = undefined)
-        }
-    });
+        .then(() => {
+            if (tokenStr != undefined) {
+                let url = API + '/activate';
+                let form = JSON.stringify({
+                    id: tokenStr
+                });
+                let opt = 'activate';
+                doRequest(url, method, form, opt)
+                    .then(() => tokenStr = undefined)
+            }
+        });
 })
 
-const doRequest = (url, method, data, options) => new Promise ((resolve, _reject) => {
+const doRequest = (url, method, data, options) => new Promise((resolve, _reject) => {
     resolve(
-    fetch(url, {
-        method: method,
-        body: data,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(
-        resp => resp.json()
-    ).then(
-        json => {
-            console.log(json);
-            if(options == 'signUp'){
-                if (json._id) tokenStr = json._id;
-                else alert('Такой пользователь уже зарегистрирован');
+        fetch(url, {
+            method: method,
+            body: data,
+            headers: {
+                'Content-Type': 'application/json'
             }
-            if (json.error) checkSignUp = json.error;
-            else if(options == 'activate') alert('Ваш пароль пришел на почту :)')
-            
-        }
-    ))
+        }).then(
+            resp => resp.json()
+        ).then(
+            json => {
+                console.log(json);
+                if (options == 'signUp') {
+                    if (json._id) tokenStr = json._id;
+                    else alert('Такой пользователь уже зарегистрирован');
+                }
+                if (json.error) checkSignUp = json.error;
+                else if (options == 'activate') alert('Ваш пароль пришел на почту :)')
+
+            }
+        ))
 });
 
 function formToJson(event) {
     let formData = new FormData(event);
     let object = {};
-    formData.forEach((value, key) => { object[key] = value });
+    formData.forEach((value, key) => {
+        object[key] = value
+    });
     let data = JSON.stringify(object);
 
     return data;
@@ -74,7 +78,7 @@ let formSign = document.querySelector('#loginIN')
 
 formSign.addEventListener('submit', event => {
     event.preventDefault();
-}) 
+})
 
 formSign.addEventListener('submit', function (event) {
     let log = document.getElementById("logSignIn");
@@ -86,7 +90,7 @@ formSign.addEventListener('submit', function (event) {
         password: valpass
     };
     doRequestSignIn(signInReq);
-    
+
 });
 
 function doRequestSignIn(data) {
@@ -100,9 +104,10 @@ function doRequestSignIn(data) {
     }).then(
         resp => resp.json()
     ).then(
-        json => {console.log(json)
-        token=json.token;
-    }
+        json => {
+            console.log(json)
+            token = json.token;
+        }
     );
 }
 
@@ -116,71 +121,78 @@ formComment.addEventListener('submit', event => {
 })
 let commentOut;
 
-formComment.addEventListener('submit', function(event){
+formComment.addEventListener('submit', function (event) {
     let commentAdd = document.getElementById("commentAdd");
     let parrentId = document.getElementById("parrentId");
     let valCommentAdd = commentAdd.value;
     let valParrentId = parrentId.value;
-    let comment={ message: valCommentAdd, parrentId:valParrentId};
+    let comment = {
+        message: valCommentAdd,
+        parrentId: valParrentId
+    };
     let tokenStr = token;
     doRequestComment(comment, tokenStr);
 });
 
 
-function doRequestComment(data, token){
+function doRequestComment(data, token) {
     fetch('https://intern-staging.herokuapp.com/api/comment', {
         method: 'POST',
         body: JSON.stringify(data),
-      headers:{
-          'Content-Type':'application/json',
-          'token':token
-      },
-      }).then(
-          resp=>resp.json()
-      ).then(
-          json => {console.log(json)
-          commentOut=json.message;
-          document.getElementById('comment').innerHTML=commentOut;
-        
-          }
-      );
+        headers: {
+            'Content-Type': 'application/json',
+            'token': token
+        },
+    }).then(
+        resp => resp.json()
+    ).then(
+        json => {
+            console.log(json)
+            commentOut = json.message;
+            document.getElementById('comment').innerHTML = commentOut;
+
+        }
+    );
 }
 
 /////////////////////////////////////////////////////////////
 
 
 let urlImg
-function doRequestImg(method, data, headers){
+
+function doRequestImg(method, data, headers) {
     fetch('https://intern-staging.herokuapp.com/api/file', {
         method: method,
         body: data,
-      headers: headers,
-      }).then(
-          resp=>resp.json()
-      ).then(
-          json =>{ console.log(json)
-          urlImg=json.url;
-          }
-      );
+        headers: headers,
+    }).then(
+        resp => resp.json()
+    ).then(
+        json => {
+            console.log(json)
+            urlImg = json.url;
+        }
+    );
 }
 
-let imgclass=document.querySelector('.images');
-let imgOut = document.createElement('img'); 
- imgOut.id='imgOut'; 
+let imgclass = document.querySelector('.images');
+let imgOut = document.createElement('img');
+imgOut.id = 'imgOut';
 
- imgclass.appendChild(imgOut)
+imgclass.appendChild(imgOut)
 
-// urlImg.appendChild('#imgOut');
 
 let formImg = document.forms.namedItem('imagesIN');
 
-formImg.addEventListener('submit', function(ev){
-    var formImgD=new FormData(formImg);
+formImg.addEventListener('submit', function (ev) {
+    var formImgD = new FormData(formImg);
 
     formImgD.append('parentEntityId', '1455qwe1');
 
-    doRequestImg( 'POST', formImgD, {'token': token});
+    doRequestImg('POST', formImgD, {
+        'token': token
+    });
 
-ev.preventDefault();
+    ev.preventDefault();
 
 })
